30c1ed9e460582e4168447723de984916a44a995
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-08 21:21:38 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+var colors = ["red","blue","car","green"];
+var carIndex = colors.indexOf("car");//get  "car" index
+//remove car from the colors array
+colors.splice(carIndex, 1); // colors = ["red","blue","green"]
